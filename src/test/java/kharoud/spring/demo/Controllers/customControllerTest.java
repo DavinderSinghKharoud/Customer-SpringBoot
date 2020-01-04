684a8dc21b36a1c2105e8697467a674e40cecc45
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-04 11:01:58 -0500

    Test for New Customers.

---
+++ src/test/java/kharoud/spring/demo/Controllers/customControllerTest.java
@@ -0,0 +1,72 @@
+package kharoud.spring.demo.Controllers;
+
+import kharoud.spring.demo.Model.Customer;
+import kharoud.spring.demo.Services.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.configuration.MockAnnotationProcessor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class customControllerTest {
+
+    @Mock
+    private CustomerService customerService;
+
+    @InjectMocks
+    private customerController customerController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+    }
+
+    @Test
+    public void testCustomerList() throws Exception{
+
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer());
+        customers.add(new Customer());
+        customers.add(new Customer());
+
+        //specific Mockito interaction, tell stub to return list of products
+        when(customerService.listAllCustomes()).thenReturn((List) customers);
+
+        mockMvc.perform(get("/customers/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("customers"))
+                .andExpect(model().attribute("customers", hasSize(3)));
+    }
+
+    @Test
+    public void testNewCustomer() throws Exception{
+
+        //should not call service before
+        verifyZeroInteractions(customerService);
+
+        mockMvc.perform(get("/customers/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("customerForm"))
+                .andExpect(model().attribute("customer", instanceOf(Customer.class)));
+    }
+
+}
---
+++ src/test/java/kharoud/spring/demo/Controllers/indexControllerTest.java
@@ -0,0 +1,31 @@
+package kharoud.spring.demo.Controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class indexControllerTest {
+
+    private MockMvc mockMvc;
+
+    private  IndexController indexController;
+
+    @Before
+    public void Setup(){
+        indexController = new IndexController();
+        mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+    }
+
+    @Test
+    public void testIndex() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
